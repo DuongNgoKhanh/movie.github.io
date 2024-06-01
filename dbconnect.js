07f1e3ebd957690cbd95681233b30bad2de59949
unknown <21520755@gm.uit.edu.vn>
2024-06-01 22:21:13 +0700

    Add new features to MovieWeb project

---
+++ dbconnect.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { MongoClient} = require('mongodb');
+const uri = "mongodb+srv://duong:EIRvSa9YNM9jkvwX@movieweb.tehyrrt.mongodb.net/?retryWrites=true&w=majority&appName=Movieweb";
+const client = new MongoClient(uri);
+
+const connectDB = async () => {
+    try {
+        await client.connect();
+
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error('MongoDB connection failed', error);
+        process.exit(1); // Dừng ứng dụng nếu kết nối thất bại
+    }
+};
+
+module.exports = connectDB;
