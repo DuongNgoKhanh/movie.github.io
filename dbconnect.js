da3098f562381472bbc508b07bc189ca28b4a1ab
DuongNgoKhanh <108381886+DuongNgoKhanh@users.noreply.github.com>
2024-06-03 09:31:15 +0700

    Update dbconnect.js

--- dbconnect.js
+++ dbconnect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { MongoClient} = require('mongodb');
-const uri = "mongodb+srv://duong:EIRvSa9YNM9jkvwX@movieweb.tehyrrt.mongodb.net/?retryWrites=true&w=majority&appName=Movieweb";
+//const uri = "mongodb+srv://duong:EIRvSa9YNM9jkvwX@movieweb.tehyrrt.mongodb.net/?retryWrites=true&w=majority&appName=Movieweb";
+const uri = process.env.mongoURI;
 const client = new MongoClient(uri);
 
 const connectDB = async () => {
