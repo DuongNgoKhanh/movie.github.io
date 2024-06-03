63e01e3d3a7bee5842bb7c4ecd1612e06cafc443
DuongNgoKhanh <108381886+DuongNgoKhanh@users.noreply.github.com>
2024-06-03 11:22:55 +0700

    Update dbconnect.js

--- dbconnect.js
+++ dbconnect.js
@@ -1,24 +1,18 @@
 const mongoose = require('mongoose');
-const { MongoClient} = require('mongodb');
-//const uri = "mongodb+srv://duong:EIRvSa9YNM9jkvwX@movieweb.tehyrrt.mongodb.net/?retryWrites=true&w=majority&appName=Movieweb";
-const uri = process.env.mongoURI;
-//const client = new MongoClient(uri);
-const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    connectTimeoutMS: 60000, // 30 giây
-    socketTimeoutMS: 60000 // 45 giây
-});
 
+// URL kết nối đến MongoDB
+const uri = process.env.mongoURI;
 const connectDB = async () => {
     try {
-        await client.connect();
-
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('MongoDB Anh quoc connected');
     } catch (error) {
         console.error('MongoDB connection failed', error);
         process.exit(1); // Dừng ứng dụng nếu kết nối thất bại
     }
 };
-mongoose.set('debug', true);
+
 module.exports = connectDB;
